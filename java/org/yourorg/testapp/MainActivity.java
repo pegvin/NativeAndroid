c6d6a71983a69648d518b7dca5fe757e811ff1d5
Aditya <75035219+pegvin@users.noreply.github.com>
2025-06-20 00:54:16 +0530

    Add Toast

--- java/org/yourorg/testapp/MainActivity.java
+++ java/org/yourorg/testapp/MainActivity.java
@@ -3,6 +3,7 @@ package org.yourorg.testapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 
 public class MainActivity extends Activity {
@@ -12,13 +13,20 @@ public class MainActivity extends Activity {
 
 	public native String getMessage();
 
+	void ShowToast(String s) {
+		Toast toast = Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG);
+		toast.show();
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		TextView text = (TextView)findViewById(R.id.my_text);
-		text.setText(getMessage());
+		String msg = getMessage();
+		text.setText(msg);
+		ShowToast(msg);
 	}
 
 	@Override
